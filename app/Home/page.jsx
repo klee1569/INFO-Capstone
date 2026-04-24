86bd773d76229be555414dd45d9ee8b9f0cdf6e5
Hermela Milash <hermelamilash@Hermelas-MacBook-Pro.local>
2026-04-24 14:47:29 -0700

    added home page and some starter code

---
+++ app/Home/page.jsx
@@ -0,0 +1,20 @@
+
+"use client";
+import { useState } from "react";
+import Navbar from "../../components/Navbar.jsx";
+import Link from "next/link";
+
+export default function Home(){
+    return (
+        <main className="homepage">
+            <section className="hero">
+                <h1>Health Care In the U.S explained <br> <em>simply</em> </br></h1>
+                <p> S imple guide to help immigrants in Seattle and King County 
+                    area understand and navigate the U.S. healthcare system with confidence.
+                </p>
+            </section>
+
+            
+        </main>
+    )
+}
